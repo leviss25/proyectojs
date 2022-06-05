01c622114ea7e935b5136e18f212d2dbc22d6474
Carl Leviss <clmayhuap@gmail.com>
2022-06-05 07:11:05 -0500

    creamos un archivo productos.json para guardar archivos y usamos fetch para mostrar los productos y crear los eventos, ordenamos las variables y funciones

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,78 +1,20 @@
+let arrayProducts =[];
+let countitemscart = document.querySelector("#countitemscart");
 let gridProduct = document.querySelector("#gridproducts");
-// to go through the arrayProducts and create a card for each product
-arrayProducts.forEach((prod) => {
-  //we count the number of stars of each product....
-  let htmlStars = "";
-  for (let i = 1; i <= 5; i++) {
-    i <= prod.stars
-      ? (htmlStars += `<ion-icon name="star"></ion-icon>`)
-      : (htmlStars += `<ion-icon name="star-outline"></ion-icon>`);
-  }
-  //....
-  let typediscount = "";
-  switch (prod.discount) {
-    case "sale": {
-      typediscount = "angle black";
-      break;
-    }
-    case "new": {
-      typediscount = "angle pink";
-      break;
-    }
-  }
-  gridProduct.innerHTML += `
-      <div class="showcase" id="product${prod.id}">
-        <div class="showcase-banner">
-          <img src="./assets/images/products/${prod.type}.jpg" alt="${prod.description}" width="300" class="product-img default">
-          <img src="./assets/images/products/${prod.type}_1.jpg" alt="${prod.description}" width="300" class="product-img hover">
-          <p class="showcase-badge ${typediscount}">${prod.discount}</p>
-          <div class="showcase-actions" id="actions${prod.id}">
-            <button class="btn-action" id="btnHeart${prod.id}">
-              <ion-icon name="heart-outline"></ion-icon>
-            </button>
-            <button class="btn-action" id="btnEye${prod.id}">
-              <ion-icon name="eye-outline"></ion-icon>
-            </button>
-            <button class="btn-action" id="btnRepeat${prod.id}">
-              <ion-icon name="repeat-outline"></ion-icon>
-            </button>
-            <button class="btn-action" id="btnAdd${prod.id}">
-              <ion-icon name="bag-add-outline"></ion-icon>
-            </button>
-          </div>
-        </div>
-        <div class="showcase-content">
-          <a href="#" class="showcase-category">${prod.category}</a>
-          <h3>
-            <a href="#" class="showcase-title">${prod.description}</a>
-          </h3>
-          <div class="showcase-rating">
-            ${htmlStars}
-          </div>
-          <div class="price-box">
-            <p class="price">${prod.currency + prod.price}</p>
-            <del>${prod.currency + prod.startingPrice}</del>
-          </div>
-        </div>
-      </div>
-    `;
-});
-function getItemStorage() {//to update the cart with the localStorage information, we create empty arrya if there isn't information
+
+function getItemStorage() {//update the cart with the localStorage information, we create empty arrya if there isn't information
   return JSON.parse(localStorage.getItem("cart")) || [];
 }
-let arrayCart = getItemStorage();
-function countTotalProducts(){
+function countTotalProducts(){//count all products of arrayCart (localStorage)
   let total=0;
   arrayCart.forEach(p=>{
     total = total + p.count;
   })
   return total;
 }
-let countitemscart = document.querySelector("#countitemscart");
-console.log(countitemscart);
-console.log(countitemscart.textContent);
-console.log(countTotalProducts());
-countitemscart.textContent = countTotalProducts();
+function updateCountItemsCart(){//update text of cartnumber
+  countitemscart.textContent = countTotalProducts();  
+}
 function saveItemStorage(array) {
   localStorage.setItem("cart", JSON.stringify(array));
 }
@@ -80,78 +22,78 @@ function isInCart(id) {
   arrayCart = getItemStorage();
   return arrayCart.some(productito => productito.id == id);
 }
-function updatecount(id) {
+function updatecount(id) {//function to update number of items in arrayCart
   arrayCart = getItemStorage();
   const indice = arrayCart.findIndex(productito => productito.id == id);
   arrayCart[indice].count++;
-  saveItemStorage(arrayCart);
+  saveItemStorage(arrayCart);////update localstorage
 }
-function countProduct(id, array) //Count a product type from an array of products
+function priceProduct(id, array) //return price of product
 {
-  return array.find(p => p.id == id).count;
+  return array[array.findIndex(p=>p.id == id)].price;
 }
-function totalCost(){
+function totalCost(){//find total cost
   let total=0;
   arrayCart.forEach(e => {
-    total = total + (e.count * e.price);
+    total = total + (e.count * priceProduct(e.id, arrayProducts));
   })
   return total;
 }
-
 function showProductAdded(id) {
   const prod = arrayProducts[arrayProducts.findIndex(p=>p.id == id)];
   arrayCart=getItemStorage();
   const n = countTotalProducts();
   totalSum = totalCost();
-  delieryCost = totalSum >= 300 ? 0.00 : 4.99;
+  //delivery is free when it is greater than $300  :)
+  delieryCost = totalSum >= 100 ? 0.00 : 4.99;
   textDelieryCost = delieryCost == 0 ? "FREE" : "$ 4.99";
   total = totalSum + delieryCost;
   Swal.fire({
     title: '<strong>SUCCESSFULLY ADDED TO CART!</strong>',
     html:`
     <div class="modal-content2">
-    <div class="product-preview">
-      <div class="product-description">
-        <img
-        src="./assets/images/products/${prod.type}.jpg"
-        alt="${prod.description}"
-        width="100"
-        height="100"
-        />
-      </div>
-      <div class="product-description">
-        <h3 class="newsletter-title">${prod.description}</h3>
-        <p class="newsletter-desc">${prod.currency} ${prod.price}</p>
-        <p class="newsletter-desc">${prod.category}</p>
-        <p class="newsletter-desc">Quantity: 1</p>
-      </div>
-    </div>
-    <div class="product-preview">
-      <form action="#">
-        <div class="newsletter-header">
-          <h3 class="newsletter-title">YOU CART</h3>
-          <div class="content-item">
-            <p class="newsletter-desc">${n} items</p>
-          </div>
-          <div class="content-item">
-            <p class="newsletter-desc">Total product cost:</p>
-            <p class="newsletter-desc">${prod.currency} ${totalSum}</p>
-          </div>
-          <div class="content-item">
-            <p class="newsletter-desc">Delivery costs:</p>
-            <p class="newsletter-desc">${textDelieryCost}</p>
-          </div>
-          <div class="content-item">
-            <p class="newsletter-desc">Total :</p>
-            <p class="newsletter-desc">${prod.currency} ${total}</p>
-          </div>
-          <p class="newsletter-desc">(taxes included)</p>
-          </p>
+      <div class="product-preview">
+        <div class="product-description">
+          <img
+          src="./assets/images/products/${prod.type}.jpg"
+          alt="${prod.description}"
+          width="100"
+          height="100"
+          />
         </div>
-        <button type="submit" class="btn-newsletter">View Cart</button>
-      </form>
-    </div>
-  </div> 
+        <div class="product-description">
+          <h3 class="newsletter-title">${prod.description}</h3>
+          <p class="newsletter-desc">${prod.currency} ${prod.price}</p>
+          <p class="newsletter-desc">${prod.category}</p>
+          <p class="newsletter-desc">Quantity: 1</p>
+        </div>
+      </div>
+      <div class="product-preview">
+        <form action="#">
+          <div class="newsletter-header">
+            <h3 class="newsletter-title">YOU CART</h3>
+            <div class="content-item">
+              <p class="newsletter-desc">${n} items</p>
+            </div>
+            <div class="content-item">
+              <p class="newsletter-desc">Total product cost:</p>
+              <p class="newsletter-desc">${prod.currency} ${totalSum}</p>
+            </div>
+            <div class="content-item">
+              <p class="newsletter-desc">Delivery costs:</p>
+              <p class="newsletter-desc">${textDelieryCost}</p>
+            </div>
+            <div class="content-item">
+              <p class="newsletter-desc">Total :</p>
+              <p class="newsletter-desc">${prod.currency} ${total}</p>
+            </div>
+            <p class="newsletter-desc">(taxes included)</p>
+            </p>
+          </div>
+          <button type="submit" class="btn-newsletter">View Cart</button>
+        </form>
+      </div>
+    </div> 
     `,
     showCloseButton: true,
     showConfirmButton: false,
@@ -159,15 +101,144 @@ function showProductAdded(id) {
     width: '50%'
   })
 }
-//to add click event in the buttons to add products to the cart and to the localStorage
-arrayProducts.forEach((productArray) => {
-  document.querySelector(`#btnAdd${productArray.id}`).addEventListener("click", () => {
-    if (isInCart(productArray.id)) {
-      updatecount(productArray.id);
-    } else {
-      arrayCart.push({ ...arrayProducts.find(productito => productito.id == productArray.id), count: 1 });
-      saveItemStorage(arrayCart);
+fetch('products.json')
+.then(response => response.json())
+.then(productoss =>{
+  arrayProducts = productoss;
+  productoss.forEach(prod =>{
+    //count the number of stars of each product....
+    let htmlStars = "";
+    for (let i = 1; i <= 5; i++) {
+      i <= prod.stars
+        ? (htmlStars += `<ion-icon name="star"></ion-icon>`)
+        : (htmlStars += `<ion-icon name="star-outline"></ion-icon>`);
     }
-    showProductAdded(productArray.id);
+    //....
+    let typediscount = "";
+    switch (prod.discount) {
+      case "sale": {
+        typediscount = "angle black";
+        break;
+      }
+      case "new": {
+        typediscount = "angle pink";
+        break;
+      }
+    }
+    gridProduct.innerHTML += `
+        <div class="showcase" id="product${prod.id}">
+          <div class="showcase-banner">
+            <img src="./assets/images/products/${prod.type}.jpg" alt="${prod.description}" width="300" class="product-img default">
+            <img src="./assets/images/products/${prod.type}_1.jpg" alt="${prod.description}" width="300" class="product-img hover">
+            <p class="showcase-badge ${typediscount}">${prod.discount}</p>
+            <div class="showcase-actions" id="actions${prod.id}">
+              <button class="btn-action" id="btnHeart${prod.id}">
+                <ion-icon name="heart-outline"></ion-icon>
+              </button>
+              <button class="btn-action" id="btnEye${prod.id}">
+                <ion-icon name="eye-outline"></ion-icon>
+              </button>
+              <button class="btn-action" id="btnRepeat${prod.id}">
+                <ion-icon name="repeat-outline"></ion-icon>
+              </button>
+              <button class="btn-action" id="btnAdd${prod.id}">
+                <ion-icon name="bag-add-outline"></ion-icon>
+              </button>
+            </div>
+          </div>
+          <div class="showcase-content">
+            <a href="#" class="showcase-category">${prod.category}</a>
+            <h3>
+              <a href="#" class="showcase-title">${prod.description}</a>
+            </h3>
+            <div class="showcase-rating">
+              ${htmlStars}
+            </div>
+            <div class="price-box">
+              <p class="price">${prod.currency + prod.price}</p>
+              <del>${prod.currency + prod.startingPrice}</del>
+            </div>
+          </div>
+        </div>
+      `;
+  })
+  //to add click event in the buttons to add products to the cart and to the localStorage
+  arrayProducts.forEach(productArray => {
+    document.querySelector(`#btnAdd${productArray.id}`).addEventListener("click", () => {
+      if (isInCart(productArray.id)) {
+        updatecount(productArray.id);
+      } else {
+        let {id} = arrayProducts.find(productito => productito.id == productArray.id);
+        arrayCart.push({id, count: 1 });
+        // arrayCart.push({ ...arrayProducts.find(productito => productito.id == productArray.id), count: 1 });
+        saveItemStorage(arrayCart);
+      }
+      updateCountItemsCart();
+      showProductAdded(productArray.id);
+    });
   });
-});
+})
+// // to go through the arrayProducts and create a card for each product
+// arrayProducts.forEach((prod) => {
+//   //count the number of stars of each product....
+//   let htmlStars = "";
+//   for (let i = 1; i <= 5; i++) {
+//     i <= prod.stars
+//       ? (htmlStars += `<ion-icon name="star"></ion-icon>`)
+//       : (htmlStars += `<ion-icon name="star-outline"></ion-icon>`);
+//   }
+//   //....
+//   let typediscount = "";
+//   switch (prod.discount) {
+//     case "sale": {
+//       typediscount = "angle black";
+//       break;
+//     }
+//     case "new": {
+//       typediscount = "angle pink";
+//       break;
+//     }
+//   }
+//   gridProduct.innerHTML += `
+//       <div class="showcase" id="product${prod.id}">
+//         <div class="showcase-banner">
+//           <img src="./assets/images/products/${prod.type}.jpg" alt="${prod.description}" width="300" class="product-img default">
+//           <img src="./assets/images/products/${prod.type}_1.jpg" alt="${prod.description}" width="300" class="product-img hover">
+//           <p class="showcase-badge ${typediscount}">${prod.discount}</p>
+//           <div class="showcase-actions" id="actions${prod.id}">
+//             <button class="btn-action" id="btnHeart${prod.id}">
+//               <ion-icon name="heart-outline"></ion-icon>
+//             </button>
+//             <button class="btn-action" id="btnEye${prod.id}">
+//               <ion-icon name="eye-outline"></ion-icon>
+//             </button>
+//             <button class="btn-action" id="btnRepeat${prod.id}">
+//               <ion-icon name="repeat-outline"></ion-icon>
+//             </button>
+//             <button class="btn-action" id="btnAdd${prod.id}">
+//               <ion-icon name="bag-add-outline"></ion-icon>
+//             </button>
+//           </div>
+//         </div>
+//         <div class="showcase-content">
+//           <a href="#" class="showcase-category">${prod.category}</a>
+//           <h3>
+//             <a href="#" class="showcase-title">${prod.description}</a>
+//           </h3>
+//           <div class="showcase-rating">
+//             ${htmlStars}
+//           </div>
+//           <div class="price-box">
+//             <p class="price">${prod.currency + prod.price}</p>
+//             <del>${prod.currency + prod.startingPrice}</del>
+//           </div>
+//         </div>
+//       </div>
+//     `;
+// });
+let arrayCart = getItemStorage(); //update arrayCart with localstorage
+updateCountItemsCart(); // update text of cartnumber
+
+
+
+
